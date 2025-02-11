3cd8510e16c927617e059db4b3c6bd34a8fe294f
Paul Nwodo <145398513+Tonypaul544@users.noreply.github.com>
2025-02-11 08:40:07 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,91 +1,3 @@
-// const questionElement = document.getElementById("question");
-// const answerButtons = document.getElementById("answer-buttons");
-// const nextButton = document.getElementById("next-btn");
-
-// let currentQuestionIndex = 0;
-// let score = 0;
-
-// function startQuiz() {
-//   currentQuestionIndex = 0;
-//   score = 0;
-//   nextButton.innerHTML = "Next";
-//   showQuestion();
-// }
-
-// function showQuestion() {
-//   resetState();
-//   let currentQuestion = questions[currentQuestionIndex];
-//   let questionNo = currentQuestionIndex + 1;
-//   questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
-//   currentQuestion.answers.forEach((answer) => {
-//     const button = document.createElement("button");
-//     button.innerHTML = answer.text;
-//     button.classList.add("btn");
-//     answerButtons.appendChild(button);
-//     if (answer.correct) {
-//       button.dataset.correct = answer.correct;
-//     }
-//     button.addEventListener("click", selectAnswer);
-//   });
-// }
-
-// function resetState() {
-
-
-//   nextButton.style.display = "none";
-//   while (answerButtons.firstChild) {
-//     answerButtons.firstChild.removeChild(answerButtons.firstChild);  //(answerButtons.firstChild);
-//   // }
-// }
-
-// // function resetState(){
-// //   nextButton.style.display = "none";
-// //   while (answerButtons.firstChild) {
-// //     answerButtons.removeChild(answerButtons.firstChild);
-// //   }
-// // }
-
-// function selectAnswer(e) {
-//   const selectedBtn = e.target;
-//   const isCorrect = selectedBtn.dataset.correct === "true";
-//   if (isCorrect) {
-//     selectedBtn.classList.add("correct");
-//     score++;
-//   } else {
-//     selectedBtn.classList.add("incorrect");
-//   }
-//   Array.from(answerButtons.children).forEach((button) => {
-//     if (button.dataset.correct === "true") {
-//       button.classList.add("correct");
-//     }
-//     button.disabled = true;
-//   });
-//   nextButton.style.display = "block";
-// }
-
-// function showscore() {
-//   resetState();
-//   questionElement.innerHTML = `You scored: ${score} out of ${questions.length}!`;
-//   nextButton.innerHTML = "Play Again";
-//   nextButton.style.display = "block";
-// }
-
-// nextButton.addEventListener("click", () => {
-//   if (currentQuestionIndex < questions.length) {
-//     currentQuestionIndex++;
-//     showQuestion();
-//   } else {
-//     showscore();
-//   }
-// });
-
-// startQuiz();
-
-
-
-
-
-
 
 
 const quizData = [
@@ -225,14 +137,7 @@ function displayScore(){
 }
 
 function restart() {
-  document.getElementById("choices").style.display = "block";
-  document.getElementById("question").style.display = "block";
-  document.getElementById("prevButton").style.display = "block";
-  document.getElementById("nextButton").style.display = "block";
-  document.getElementById("changeToAnswer").innerHTML = "Your score";
-  document.getElementById("replayButton").style.display = "none";
-  document.getElementById("result-container").style.display = "none";
-
+  location.reload();
 }
 
 
